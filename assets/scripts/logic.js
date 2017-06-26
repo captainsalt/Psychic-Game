225412194b93bb6c75af8e2708466f4c2108f596
David Wright <dontbothertheking@gmail.com>
2017-06-25 21:50:28 -0500

    first commit

---
+++ assets/scripts/logic.js
@@ -0,0 +1,85 @@
+//Do imports so you can organize code
+
+var letter = randomLetter();
+var wins = 0;
+var losses = 0;
+var gameObject;
+
+initialize();
+
+//put in a differnt file
+/**
+ * @param {KeyboardEvent} e
+ */
+document.onkeydown = function (e) {
+    var letterChecker = new RegExp("[A-Z]");
+    var userInput = e.key.toUpperCase();
+
+    //if the user input isnt a letter or 
+    if (!letterChecker.exec(userInput) ||
+        gameObject.guessedCharacters.includes(userInput) && gameObject.guessesLeft != 0) {
+        alert("Please enter a letter or a letter you haven't already tried");
+        return;
+    }
+
+    //if the user doesn't guess in time
+    if (gameObject.guessesLeft <= 1) {
+        losses++;
+        alert("You lose");
+        initialize();
+        return;
+    }
+
+    if (userInput === letter) {
+        wins++;
+        alert("You win!");
+        initialize();
+        return;
+    }
+
+    gameObject.guessedCharacters.push(userInput);
+    gameObject.guessesLeft -= 1;
+    updateAllElements();
+}
+
+function initialize() {
+    letter = randomLetter();
+    gameObject = new Game();
+    updateAllElements();
+}
+
+//Game constructor
+function Game() {
+    this.wins = wins;
+    this.losses = losses;
+    this.guesses = [];
+    this.guessesLeft = 10;
+    this.guessedCharacters = [];
+    this.letter = randomLetter();
+}
+
+function randomLetter() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function updateAllElements() {
+    updateHtmlElement("wins", "Wins: " + gameObject.wins);
+    updateHtmlElement("losses", "Losses: " + gameObject.losses);
+    updateHtmlElement("guesses", "Your guesses: " + gameObject.guessedCharacters.join(", "));
+    updateHtmlElement("guessesLeft", "Guesses left: " + gameObject.guessesLeft);
+}
+
+/**
+ * 
+ * @param {string} elementId 
+ * @param {string} elementValue 
+ */
+function updateHtmlElement(elementId, elementValue) {
+    var element = $("#" + elementId);
+    element.prop("innerText", elementValue);
+}
+
